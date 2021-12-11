94bd70abb00422e829a39c144d807ebcb03f38a8
berryberry189 <berryberry189@naver.com>
2021-12-11 18:02:56 +0900

    Initial commit

---
+++ src/test/java/com/book/objects/ObjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.objects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

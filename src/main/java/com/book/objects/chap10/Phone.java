76299cd7b60256c3a5a4279839735066d7dc0850
berryberry189 <berryberry189@naver.com>
2021-12-20 11:47:30 +0900

    [chap10] 상속과 코드 재사용 - 중복 줄이는 방법 1.Phone 과 NightlyDiscountPhone 를 한 클래스로 합치기 => type 으로 분리

--- src/main/java/com/book/objects/chap10/Phone.java
+++ src/main/java/com/book/objects/chap10/Phone.java
@@ -7,17 +7,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Phone {
+
+    private static final int LATE_NIGHT_HOUR = 22;
+    enum PhoneType { REGULAR, NIGHTLY }
+
+    private PhoneType type;
+
     // 단위 요금
     private Money amount;
+    // 심야 할인 요금 ( LATE_NIGHT_HOUR 이후 적용 )
+    private Money nightlyAmount;
+    // 일반 요금 ( LATE_NIGHT_HOUR 이전 적용 )
+    private Money regularAmount;
     // 단위 시간
     private Duration seconds;
     // 전체 통화 목록
     private List<Call> calls = new ArrayList<>();
-
-    public Phone(Money amount, Duration seconds) {
-        this.amount = amount;
-        this.seconds = seconds;
-    }
 
     public void call(Call call) {
         calls.add(call);
@@ -35,10 +40,35 @@ public class Phone {
         return seconds;
     }
 
+    public Phone(Money amount, Duration seconds) {
+        this(PhoneType.REGULAR, amount, Money.ZERO, Money.ZERO, seconds);
+    }
+
+    public Phone(Money nightlyAmount, Money regularAmount, Duration seconds) {
+        this(PhoneType.REGULAR, Money.ZERO, nightlyAmount, regularAmount, seconds);
+    }
+
+    public Phone(PhoneType type, Money amount, Money nightlyAmount, Money regularAmount, Duration seconds) {
+        this.type = type;
+        this.amount = amount;
+        this.nightlyAmount = nightlyAmount;
+        this.regularAmount = regularAmount;
+        this.seconds = seconds;
+    }
+
     public Money calculateFee() {
         Money result = Money.ZERO;
+
         for (Call call : calls) {
-            result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            if(type == PhoneType.REGULAR){
+                result = result.plus(amount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+            } else {
+                if(call.getFrom().getHour() >= LATE_NIGHT_HOUR){
+                    result = result.plus(nightlyAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+                }else{
+                    result = result.plus(regularAmount.times(call.getDuration().getSeconds() / seconds.getSeconds()));
+                }
+            }
         }
         return result;
     }

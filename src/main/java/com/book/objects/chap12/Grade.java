b560677525479cf625c73a7a3b690a2ed0c577ba
berryberry189 <berryberry189@naver.com>
2021-12-23 23:03:45 +0900

    [chap12] 상속을 사용한 강의평가 - 상속을 통해 Lecture 재사용

---
+++ src/main/java/com/book/objects/chap12/Grade.java
@@ -0,0 +1,29 @@
+package com.book.objects.chap12;
+
+/* 등급 */
+public class Grade {
+
+    // 등급명
+    private String name;
+    // 최소 성적, 최대 성적
+    private int upper, lower;
+
+    private Grade(String name, int upper, int lower){
+        this.name = name;
+        this.upper = upper;
+        this.lower = lower;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isName(String name){
+        return this.name.equals(name);
+    }
+
+    public boolean include(int score){
+        return score >= lower && score <= upper;
+    }
+
+}
---
+++ src/main/java/com/book/objects/chap12/GradeLecture.java
@@ -0,0 +1,63 @@
+package com.book.objects.chap12;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/* 등급별 통계 */
+public class GradeLecture extends Lecture{
+
+    private List<Grade> grades;
+
+    public GradeLecture(String title, int pass, List<Integer> scores, List<Grade> grades) {
+        super(title, pass, scores);
+        this.grades = grades;
+    }
+
+    /**
+     * 자식 클래스와 부모 클래스에 완전히 동일한 시그니처를 가진 메서드가 존재할 경우
+     * 자식 클래스의 메서드 우선순위가 더 높다
+     * => 오버라이딩
+     */
+    @Override
+    public String evaluate(){
+        return super.evaluate() + ", " + gradeStatistics();
+    }
+
+    private String gradeStatistics() {
+        return grades.stream()
+                .map(grade -> format(grade))
+                .collect(Collectors.joining(" "));
+    }
+
+    private String format(Grade grade) {
+        return String.format("%s:%d", grade.getName(), gradeCount(grade));
+    }
+
+    private long gradeCount(Grade grade) {
+        return getScores().stream()
+                .filter(grade::include)
+                .count();
+    }
+
+    /**
+     * 등급별 평균 구하기
+     * (부모 클래스 Lecture 의 average 메서드와는 시그니처가 달라서 공존 가능 => 메서드 오버로딩 )
+     * @param gradeName
+     * @return
+     */
+    public double average(String gradeName){
+        return grades.stream()
+                .filter(each -> each.isName(gradeName))
+                .findFirst()
+                .map(this::gradeAverage)
+                .orElse(0d);
+    }
+
+    private double gradeAverage(Grade grade) {
+        return getScores().stream()
+                .filter(grade::include)
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0);
+    }
+}
